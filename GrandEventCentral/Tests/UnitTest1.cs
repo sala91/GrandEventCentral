c20b32563ae87b3ddddb36a6cf4e9f1768bb99ea
Sander Soots <sander.soots@eesti.ee>
2020-05-13 15:49:17 +0300

    Adds unit testing capabilities

--- GrandEventCentral/Tests/UnitTest1.cs
+++ GrandEventCentral/Tests/UnitTest1.cs
@@ -1,6 +1,10 @@
 using System;
 using Xunit;
 
+// Authors Twitter: https://twitter.com/stevensanderson
+// Documentation: https://github.com/SteveSandersonMS/BlazorUnitTestingPrototype/tree/master/SampleApp.Tests
+// Blog post about this test framework: https://blog.stevensanderson.com/2019/08/29/blazor-unit-testing-prototype/
+// Future work under task: https://github.com/dotnet/aspnetcore/issues/5458
 namespace GrandEventCentral.Test
 {
     public class UnitTest1

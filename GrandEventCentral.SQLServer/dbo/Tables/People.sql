4d8cd3a79670310814a0f91b15e26bbb57707c5b
Sander Soots <ssoots@bhsolutions.com>
2020-05-22 14:44:47 +0300

    adding sql server project

---
+++ GrandEventCentral.SQLServer/dbo/Tables/People.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[People] (
+    [Id]            UNIQUEIDENTIFIER NOT NULL,
+    [CreatorId]     NVARCHAR (450)   NULL,
+    [FirstName]     NVARCHAR (10)    NOT NULL,
+    [LastName]      NVARCHAR (15)    NOT NULL,
+    [Name]          NVARCHAR (MAX)   NULL,
+    [Email]         NVARCHAR (MAX)   NOT NULL,
+    [Gender]        INT              NULL,
+    [StartDate]     DATETIME2 (7)    NOT NULL,
+    [PreferredTeam] NVARCHAR (MAX)   NOT NULL,
+    [Biography]     NVARCHAR (MAX)   NULL,
+    [Picture]       NVARCHAR (MAX)   NULL,
+    [DateOfBirth]   DATETIME2 (7)    NOT NULL,
+    CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_People_AspNetUsers_CreatorId] FOREIGN KEY ([CreatorId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_People_CreatorId]
+    ON [dbo].[People]([CreatorId] ASC);
+
---
+++ GrandEventCentral.SQLServer/dbo/Tables/PublicEvents.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[PublicEvents] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [CreatorId]   NVARCHAR (450)   NULL,
+    [Title]       NVARCHAR (MAX)   NULL,
+    [Description] NVARCHAR (MAX)   NULL,
+    [HostName]    NVARCHAR (MAX)   NULL,
+    [EventUrlFb]  NVARCHAR (MAX)   NULL,
+    [Date]        DATETIME2 (7)    NULL,
+    [CreatedAt]   DATETIME2 (7)    NULL,
+    [FullAddress] NVARCHAR (MAX)   NULL,
+    [Latitude]    REAL             NULL,
+    [Longitude]   REAL             NULL,
+    CONSTRAINT [PK_PublicEvents] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_PublicEvents_AspNetUsers_CreatorId] FOREIGN KEY ([CreatorId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_PublicEvents_CreatorId]
+    ON [dbo].[PublicEvents]([CreatorId] ASC);
+
